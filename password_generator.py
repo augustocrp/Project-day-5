86c5258a4272ba1b2aeddd6a12624c2bc0ed4db2
Augusto Cesar <dev.augusto.crp@gmail.com>
2022-12-21 23:29:43 -0300

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,11 +1,26 @@
 import random
 import string
 
-
 def password_generator(Len_pass = 8):
     ascii_options = string.ascii_letters
     number_options = string.digits
     punt_options = string.punctuation
     options = ascii_options + number_options + punt_options
-    
-    
+
+    password_user = ""
+    for i in range(0, Len_pass):
+        digit = random.choice(options)
+        password_user = password_user + digit
+
+    return password_user
+
+choice_user = input("Quantos digitos na senha?")
+
+if choice_user.isdigit():
+    choice_user = int(choice_user)
+else:
+    print("Entrada inválida!")
+    quit()
+
+password_generator(Len_pass =choice_user)
+        
